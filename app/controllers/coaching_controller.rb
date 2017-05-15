f7d2d9679fae9ba9ac4847afe22349fc52bd1987
Vincent Panis <vincent@come2link.com>
2017-05-15 12:35:27 +0200

    stupid coach online

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -2,9 +2,30 @@ class CoachingController < ApplicationController
 
 def answer
   @query = params[:query]
+  @answer = coach_answer_enhanced(@query)
 end
 
 def ask
+end
+
+def coach_answer(your_message)
+  if @query.include?("?")
+    return "Silly question, get dressed and go to work!"
+  elsif your_message.upcase == "I AM GOING TO WORK RIGHT NOW!"
+    return ""
+  else
+    return "I don't care, get dressed and go to work!"
+  end
+end
+
+def coach_answer_enhanced(your_message)
+  if your_message.upcase == "I AM GOING TO WORK RIGHT NOW!"
+    return ""
+  elsif your_message == your_message.upcase
+    return "I can feel your motivation! #{coach_answer(your_message)}"
+  else
+    return coach_answer(your_message)
+  end
 end
 
 end
